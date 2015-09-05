ee0a3fb56f1fd466d3001a7fc45eb9a044e12c52
Peter Scheele <peter@peter-scheele.nl>
2015-09-05 12:27:14 +0200

    commit

--- generate-config.py
+++ generate-config.py
@@ -1,7 +1,11 @@
 # -- coding: utf-8 --
 
+#sort out regexes in replacements!!!
+
+
 #get current directory
 import os
+import re
 path = os.path.dirname(os.path.abspath(__file__))
 
 #load customizations
@@ -13,9 +17,31 @@ template = open('%s/template.cfg' % path,'r')
 workingtemplate = template.read()
 template.close()
 
+#load customizations
+
+
+i=0
+customization = []
+customizations = open('%s/customizations.cfg' % path, 'r')
+for line in customizations:
+	temp = [line.replace("\n", "")]
+	customization.append(temp)
+	print customization[i]
+	i +=1
+
+
+customizations.close()
+
 #customize template
-workingtemplate = workingtemplate.replace("!!IP!!", '127.0.0.1')
-workingtemplate = workingtemplate.replace('! !IP! !', '!!IP!!')
+
+i=0
+reg_before = "(=.*$|\[\')"
+reg_after = "(^.*=|\'\])"
+for replace in customization:
+	temp = "%s" % customization[i]
+	print "translate %s  to %s" % (re.sub('%s' % reg_before,'', temp), re.sub('%s' % reg_after,'', temp))
+	workingtemplate = workingtemplate.replace(re.sub('%s' % reg_before,'', temp), re.sub('%s' % reg_after,'', temp))
+	i +=1
 
 #write config file
 config = open('%s/%s.cfg' % (path, hostname) ,'w')

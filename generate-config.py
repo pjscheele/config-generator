2d16f235524ff8607c8fe1e1790af96c0297252a
Peter Scheele <peter@peter-scheele.nl>
2015-09-06 17:13:05 +0200

    converted to use fuctions

--- generate-config.py
+++ generate-config.py
@@ -1,52 +1,72 @@
 # -- coding: utf-8 --
 
-#sort out regexes in replacements!!!
+### fix ChangeTemplate
 
-
-#get current directory
+#imports
 import os
 import re
-path = os.path.dirname(os.path.abspath(__file__))
+
+#define variables
+CUSTOMIZATION = []
+TEMPLATE = ''
+CONFIG = ""
+path = os.path.dirname(os.path.abspath(__file__)) #get current directory
+
+#Define functions
+#load template
+def LoadTemplate():
+	f = open('%s/template.cfg' % path,'r')
+	template = f.read()
+	f.close()
+	return template
 
 #load customizations
-hostname = "RTEM00000FW001"
+def LoadCustomizations():
+	i=0
+	customization = []
+	customizations = open('%s/customizations.cfg' % path, 'r')
+	for line in customizations:
+		temp = [line.replace("\n", "")]
+		customization.append(temp)
+		i +=1
 
+	customizations.close()
+	return customization
 
-#open template
-template = open('%s/template.cfg' % path,'r')
-workingtemplate = template.read()
-template.close()
+#change template
+def ChangeTemplate(template1, array1):
+	i=0
+	reg_before = "(=.*$|\[\')"
+	reg_after = "(^.*=|\'\])"
+	for custom in array1:
+		temp = "%s" % array1[i]
+		template1 = template1.replace(re.sub( reg_before,'', temp), re.sub( reg_after,'', temp))
+		i +=1
 
-#load customizations
+	return template1
 
+def WriteConfig(string1,string2):
+	#write config file
+	config = open('%s/output.cfg' % string2 ,'w')
+	config.write("%s" % string1)
+	config.close()
 
-i=0
-customization = []
-customizations = open('%s/customizations.cfg' % path, 'r')
-for line in customizations:
-	temp = [line.replace("\n", "")]
-	customization.append(temp)
-	print customization[i]
-	i +=1
+	return 1
 
+#Main program
+os.system('clear')	#Clear screen
 
-customizations.close()
+print "Loading template..."
+TEMPLATE = LoadTemplate()
 
-#customize template
+print "Loading customizations..."
+CUSTOMIZATION = LoadCustomizations()
 
-i=0
-reg_before = "(=.*$|\[\')"
-reg_after = "(^.*=|\'\])"
-for replace in customization:
-	temp = "%s" % customization[i]
-	print "translate %s  to %s" % (re.sub('%s' % reg_before,'', temp), re.sub('%s' % reg_after,'', temp))
-	workingtemplate = workingtemplate.replace(re.sub('%s' % reg_before,'', temp), re.sub('%s' % reg_after,'', temp))
-	i +=1
+print "Creating config..."
+CONFIG = ChangeTemplate( TEMPLATE, CUSTOMIZATION )
 
-#write config file
-config = open('%s/%s.cfg' % (path, hostname) ,'w')
-config.write("%s" % workingtemplate)
-config.close()
+print "writing config...\n"
+WriteConfig(CONFIG, path)
 
 #done
-print "done, your config %s.cfg is in %s" % (hostname, path)
+print "done, your config output.cfg is in %s" % path
